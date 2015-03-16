f30da33bd0adf3a734f0ba62b81426ccb9727ea6
sk1LLb0X <sk1LLb0X123@gmail.com>
2015-03-16 17:03:39 -0300

    fixed some bugs, changed version to 0.4 and added code to rename main exe file

--- build.py
+++ build.py
@@ -1,11 +1,30 @@
+import os
 from py2exe.build_exe import py2exe
 from distutils.core import setup
 
 setup(
   name        = 'Sensitivity Tools',
-  version     = '0.2',
+  version     = '0.4',
   author      = 'sk1LLb0X',
-  windows     = ['main.pyw'],
-  options     = {'py2exe': {'bundle_files': 1, 'compressed': True, 'includes':['sip']}},
-  zipfile     = None
+  windows     = [{ 
+    'script' : 'main.pyw',
+    'icon_resources' : [(1, 'mouse.ico')]
+  }],
+  options     = {
+    'py2exe': {
+      'bundle_files': 3, 
+      'compressed': True,
+      'includes': [
+        'sip', 
+        'PyQt4.QtCore', 
+        'PyQt4.QtGui', 
+        'PyQt4.QtNetwork'
+      ]
+    }
+  },
+  zipfile     = None,
+  data_files  = ['history.html', 'history.css']
 )
+
+os.remove('dist/SensitivityTools.exe')
+os.rename('dist/main.exe', 'dist/SensitivityTools.exe')
